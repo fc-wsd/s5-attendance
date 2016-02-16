487687c36e1207b6063145f566ee77ecdd284fdd
chachooon <chachooon@gmail.com>
2016-02-16 12:54:18 +0900

    차정윤_hw1

--- chachooon_attendance1.py
+++ chachooon_attendance1.py
@@ -2,9 +2,8 @@
 from datetime import datetime
 
 #학생명단
-students = ['김용민', '김종록', '박준영', '박한성', '백성훈',
-'서인용', '서자랑', '손형우', '신승호', '신호철', '양정길', '윤영식',
-'이승현', '이효준', '장한별', '조혜연', '차정윤', '최현덕', '한정현']
+students = ['김용민', '김종록', '박준영', '박한성', '백성훈', '서인용', '서자랑', '손형우', '신승호',
+'신호철', '양정길', '윤영식','이승현', '이효준', '장한별', '조혜연', '차정윤', '최현덕', '한정현']
 
 #출석자명단
 attendances=[]
@@ -12,7 +11,7 @@ attendances=[]
 #출석처리함수
 def attend(student):
     time = datetime.now()
-    attendance = [student, time]
+    attendance = [student, time] #출석자 = [이름, 시간]
     if student in students:
         if student in attendences:
             print('이미 출첵 하셨어요.')
